5a0ba3c841138d087d74c684db6f8f36651ca012
Jayesh Nerpagar <jayesh.nerpagar@accelya.com>
2025-10-17 16:27:12 +0530

    test: single number input should return the number itself (failing)

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,3 +4,7 @@ test('should return o for an empty string', ()=> {
  expect(add('')).toBe(0);
 })
 
+test('should return the number itself for a single number input',()=>{
+  expect(add('1')).toBe(1);
+  expect(add('42')).toBe(42);
+})
