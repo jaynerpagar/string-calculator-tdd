91d253b8158ab685f189b53939eb81da9b6932ab
Jayesh Nerpagar <jayesh.nerpagar@accelya.com>
2025-10-17 17:33:26 +0530

    refactor: simplify add() logic for single and two numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,13 +4,13 @@ function add(input){  //changing the passing parameters from number to input for
  //if input is empty then return 0   
  if(input === '') return 0;
 
- if(input.includes(',')){
-    const numbers = input.split(',').map(Number);
-    return numbers.reduce((sum,num)=> sum + num,0);
- }
-
- // Returns 0 for empty string, otherwise returns the number itself
- return Number(input);
+ //If input contains a comma, split and sum the numbers (handle single number and multiple number)
+  const numbers = input.split(',').map(Number);
+   
+   
+ //sum all numbers 
+  return numbers.reduce((sum,num)=> sum + num,0);
+ 
 }
 
 module.exports = { add };
