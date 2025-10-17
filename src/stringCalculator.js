f947054bca788fce34afa1fa356103b90f43259d
Jayesh Nerpagar <jayesh.nerpagar@accelya.com>
2025-10-17 17:27:59 +0530

    feat: sum two numbers separated by comma

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,11 +1,16 @@
 
 
-function add(numbers){
+function add(input){  //changing the passing parameters from number to input for better understanding 
  //if input is empty then return 0   
- if(numbers === '') return 0;
+ if(input === '') return 0;
+
+ if(input.includes(',')){
+    const numbers = input.split(',').map(Number);
+    return numbers.reduce((sum,num)=> sum + num,0);
+ }
 
  // Returns 0 for empty string, otherwise returns the number itself
- return Number(numbers);
+ return Number(input);
 }
 
 module.exports = { add };
