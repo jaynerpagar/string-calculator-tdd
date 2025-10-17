5b1eac9c80ec3797012f70a723c53d9a0c5a607a
Jayesh Nerpagar <jayesh.nerpagar@accelya.com>
2025-10-17 16:38:17 +0530

    test: sum two numbers separated by comma (failing)

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -2,9 +2,15 @@ const { add } = require("../src/stringCalculator");
 
 test('should return o for an empty string', ()=> {
  expect(add('')).toBe(0);
-})
+});
 
 test('should return the number itself for a single number input',()=>{
   expect(add('1')).toBe(1);
   expect(add('42')).toBe(42);
-})
+});
+
+test('should return the sum of two numbers separated by comma',()=>{
+    expect(add('1,2')).toBe(3);
+    expect(add('200,500')).toBe(700);
+});
+
