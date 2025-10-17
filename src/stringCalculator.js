0fdabd043e4062bb025464ffcafc655150afdafd
Jayesh Nerpagar <jayesh.nerpagar@accelya.com>
2025-10-17 16:30:58 +0530

    feat: return number itself for single number input to pass test

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,11 @@
 
 
 function add(numbers){
- return 0;
+ //if input is empty then return 0   
+ if(numbers === '') return 0;
+
+ //if input is a single number then return that number 
+ return Number(numbers);
 }
 
 module.exports = { add };
