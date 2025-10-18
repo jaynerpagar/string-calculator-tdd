1c90d62b8b2681acdd06c15ae894f2aa577e5cb3
Jayesh Nerpagar <jayesh.nerpagar00@gmail.com>
2025-10-18 16:34:46 +0530

    refactor: improve add() readability and handle minor edge cases

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,7 +4,12 @@ function add(input){  //changing the passing parameters from number to input for
  //if input is empty then return 0   
  if(input === '') return 0;
 
- const number = input.replace(/\n/g, ',').split(',').map(Number);
+const normalise  = input.replace(/\n/g, ','); //normalizing the input by replacing new line with comma
+
+const number = normalise.split(',') //splitting the input string into array of numbers
+              .map(num => num.trim()) //trimming any extra spaces
+              .filter(num => num !== '') //filtering out any empty strings
+              .map(Number); //converting string numbers to integers
 
   // Convert input string to array of numbers and sum them
 return number.reduce((sum,num)=> sum + num,0);
