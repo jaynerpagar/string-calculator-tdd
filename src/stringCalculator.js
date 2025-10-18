e3746af45c25e3aa58c31e55c3cfc98a7cd38f35
Jayesh Nerpagar <jayesh.nerpagar00@gmail.com>
2025-10-18 17:15:01 +0530

    feat: ignore numbers greater than 1000 in sum

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,4 +1,6 @@
-function parseDelimiter(input) {
+
+
+function parseDelimiter(input) {  // parse custom delimiter if present 
   let delimiter = /,|\n/; // default
   let numbersPart = input;
 
@@ -16,7 +18,7 @@ function parseDelimiter(input) {
   return { delimiter, numbersPart };
 }
 
-function splitNumbers(numbersPart, delimiter) {
+function splitNumbers(numbersPart, delimiter) {// split numbers based on delimiter
   return numbersPart
     .split(delimiter)
     .map(num => num.trim())
@@ -24,7 +26,7 @@ function splitNumbers(numbersPart, delimiter) {
     .map(Number);
 }
 
-function checkNegatives(numbers) {
+function checkNegatives(numbers) { // check for negative numbers
   const negatives = numbers.filter(n => n < 0);
   if (negatives.length > 0) {
     throw new Error(`Negatives not allowed: ${negatives.join(',')}`);
@@ -34,11 +36,13 @@ function checkNegatives(numbers) {
 function add(input) {
   if (input === '') return 0;
 
-  const { delimiter, numbersPart } = parseDelimiter(input);
-  const numbers = splitNumbers(numbersPart, delimiter);
-
+  const { delimiter, numbersPart } = parseDelimiter(input); // get delimiter and numbers part
+  let numbers = splitNumbers(numbersPart, delimiter); // split numbers (const creating problem for filter and map)
+//check negative 
   checkNegatives(numbers);
 
+   // Ignore numbers greater than 1000
+  numbers = numbers.filter(n => n <= 1000);
 
   return numbers.reduce((sum, num) => sum + num, 0);
 
