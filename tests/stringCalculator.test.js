39c9c901bd2147314a38d29b627030e18d1488a0
Jayesh Nerpagar <jayesh.nerpagar00@gmail.com>
2025-10-18 16:16:29 +0530

    test: handle newline (\n) as a delimiter along with commas

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -18,3 +18,8 @@ test('should sum multiple numbers separated by commas', ()=>{
     expect(add('1,2,3,4')).toBe(10);
     expect(add('5,10,15')).toBe(30);
 })
+
+test('should handle newline as delimeter along with commas',()=>{
+  expect(add('1\n2,3')).toBe(6);
+  expect(add('10\n20\n30')).toBe(60);
+})
