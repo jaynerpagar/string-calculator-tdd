be3643eaa75ec411ae462c36678d432f4742b877
Jayesh Nerpagar <jayesh.nerpagar00@gmail.com>
2025-10-18 17:06:01 +0530

    feat: throw error for negative numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -30,6 +30,12 @@ function add(input) {
   const { delimiter, numbersPart } = parseDelimiter(input);
   const numbers = splitNumbers(numbersPart, delimiter);
 
+  const negatives = numbers.filter(n => n < 0);
+  if (negatives.length > 0) {
+    throw new Error(`Negatives not allowed: ${negatives.join(',')}`);
+  }
+
+
   return numbers.reduce((sum, num) => sum + num, 0);
 
 }
